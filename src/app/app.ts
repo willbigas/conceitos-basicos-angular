0b8a0bfb6a94fb247168eaf63dea558e854883d8
William Bigas Mauro <ice_wmauro@uolinc.com>
2025-12-11 09:39:21 -0300

    criando componente angular manualmente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloworld/helloworld.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'helloworld',
+  template: '<h1>Hello World 2</h1>',
+  styleUrls: ['./helloworld.component.css']
+})
+export class HelloWorldComponent {
+
+}
